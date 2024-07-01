5de8d68e38c451c3fbf1e2f5d6c099b324546e55
Ariel Wasserman <ariel@cfee.lan>
2024-07-01 12:31:07 -0300

    loading random icon logic and pngs

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,19 +4,28 @@ import { useEffect, useState } from 'react';
 
 function Loading() {
 
+ 
+  var randomLoadingIcon = Math.floor(Math.random() * 5);
+  //console.log(randomLoadingIcon)
+  
+  
+
   useEffect(() => {
-
-
   }, [])
-
-
-
 
   return (
 
-
-        <div>
-      <div className='loading'>
+      <div>
+        
+      <div className=
+      {(randomLoadingIcon === 0 ? 'lod loading0' :
+        randomLoadingIcon === 1 ? 'lod loading1' :
+        randomLoadingIcon === 2 ? 'lod loading2' :
+        randomLoadingIcon === 3 ? 'lod loading3' : 'lod loading4'
+        //randomLoadingIcon === 4 ? 'loading4' :
+        //randomLoadingIcon === 5 ? 'loading5' : 'loading6'
+        )}
+      >
       <div className="lds-spinner">
         <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
       </div>
